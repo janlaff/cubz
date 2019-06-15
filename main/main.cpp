995bb7c0ebec6faee8966503081b28c5ddeeadbe
Jan Lafferton <jla@jcbachmann.de>
2019-06-15 16:18:36 +0200

    Add triangle example

--- main/main.cpp
+++ main/main.cpp
@@ -16,7 +16,28 @@ int main() {
         auto ctx = graphics::OpenGLContext(800, 600, "Cubz - Experimental Version");
         auto window = ctx.getWindow();
 
+        GLuint vertexArrayId;
+        glGenVertexArrays(1, &vertexArrayId);
+        glBindVertexArray(vertexArrayId);
+
+        std::array<GLfloat, 9> vertexData {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f
+        };
+
+        GLuint vertexBuffer;
+        glGenBuffers(1, &vertexBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData.data(), GL_STATIC_DRAW);
+
         while (!glfwWindowShouldClose(window)) {
+            glEnableVertexAttribArray(0);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+            glDisableVertexAttribArray(0);
+
             glfwSwapBuffers(window);
             glfwPollEvents();
         }
